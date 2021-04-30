349179ec7bb3e218c4d23ec36d46a70dbee03109
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2021-04-30 14:25:22 +0300

    ✨ Added Canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
 import React from 'react'
-import tw from 'twin.macro'
+import 'twin.macro'
 import Navbar from './components/Navbar'
+import Pad from './components/Pad'
 
-const Title = tw.h1`
-  font-extrabold text-8xl
-`
-const Subtitle = tw.h2`
-  font-bold text-3xl text-center text-gray-300
-`
-const styles = {
-  // Move long class sets out of jsx to keep it scannable
-  container: ({ hasBackground }) => [
-    tw`justify-center h-screen`,
-    hasBackground && tw`bg-gradient-to-b from-green-500 to-blue-600 animate-gradient-y`,
-  ],
-}
 
 const App = () => (
-  <div css={styles.container({ hasBackground: true })}>
+  <div tw="bg-black">
     <Navbar />
+    <Pad />
+
   </div>
+
 )
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,14 @@ const Navbar = () => {
     const Button = tw.button`
     px-4 py-2 font-medium text-white rounded-md
     capitalize transition-colors duration-200 transform
-    font-bold text-lg mx-1
+    font-bold text-lg mx-1 
     bg-gray-800 hover:bg-gray-700
     focus:(outline-none bg-blue-800)
     `;
     return (
         <nav tw="bg-gray-900 shadow text-white">
             <div tw="container px-2 py-3 md:flex">
-                <div tw="flex items-center justify-between">
+                <div tw="items-center justify-between">
                     <Button><BiCut /></Button>
                     <Button><BiPencil /></Button>
                     <Button><BiCrop /></Button>
